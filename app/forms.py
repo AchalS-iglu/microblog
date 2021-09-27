0c6a421432f1e3e91e88e6c0ddce6718d6fa5b5c
AchalS-iglu <iithebeastx666@gmail.com>
2021-09-27 21:00:53 +0530

    Chapter 10 Email Support and Reset Password finished.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from typing import Text
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms import validators
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
@@ -52,4 +53,13 @@ class PostForm(FlaskForm):
     post = TextAreaField('Say something', validators = [
         DataRequired(), Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
 
